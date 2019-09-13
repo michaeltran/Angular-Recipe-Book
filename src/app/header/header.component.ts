546114be38bb5dc811b8b1f6257281608e0bd2af
Michael Tran <michaeltranx@gmail.com>
2019-09-13 10:16:51 -0500

    Finish HTTP module

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { DataStorageService } from 'src/shared/data-storage.service';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+    constructor(private dataStorageService: DataStorageService) {}
 
+    onSaveData() {
+        this.dataStorageService.storeRecipes();
+    }
+
+    onFetchData() {
+        this.dataStorageService.fetchRecipes().subscribe();
+    }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,24 +9,33 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe('Tasty Schnitzel',
-            'A super-tasty Schnitzel - just awesome!',
-            'https://www.thespruceeats.com/thmb/dFLHcwFFtDguBZGylUlDN8KaNaw=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/wiener-schnitzel-recipe-1447089-Hero-5b587d6c46e0fb0071b0059d.jpg',
-            [
-                new Ingredient('Meat', 1),
-                new Ingredient('French Fries', 20)
-            ]),
-        new Recipe('Big Fat Burger',
-            'What else do you need to say??',
-            'https://media1.s-nbcnews.com/j/newscms/2019_21/2870431/190524-classic-american-cheeseburger-ew-207p_d9270c5c545b30ea094084c7f2342eb4.fit-2000w.jpg',
-            [
-                new Ingredient('Buns', 2),
-                new Ingredient('Meat', 1)
-            ])
-    ];
+    // private recipes: Recipe[] = [
+    //     new Recipe('Tasty Schnitzel',
+    //         'A super-tasty Schnitzel - just awesome!',
+    // tslint:disable-next-line: max-line-length
+    //         'https://www.thespruceeats.com/thmb/dFLHcwFFtDguBZGylUlDN8KaNaw=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/wiener-schnitzel-recipe-1447089-Hero-5b587d6c46e0fb0071b0059d.jpg',
+    //         [
+    //             new Ingredient('Meat', 1),
+    //             new Ingredient('French Fries', 20)
+    //         ]),
+    //     new Recipe('Big Fat Burger',
+    //         'What else do you need to say??',
+    // tslint:disable-next-line: max-line-length
+    //         'https://media1.s-nbcnews.com/j/newscms/2019_21/2870431/190524-classic-american-cheeseburger-ew-207p_d9270c5c545b30ea094084c7f2342eb4.fit-2000w.jpg',
+    //         [
+    //             new Ingredient('Buns', 2),
+    //             new Ingredient('Meat', 1)
+    //         ])
+    // ];
+
+    private recipes: Recipe[] = [];
 
     constructor(private slService: ShoppingListService) {}
+
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
+    }
 
     getRecipes() {
         return this.recipes.slice();
